fb186c54e4c89ec22134ba26b35204a33c97adbe
VelkoM01 <87763837+VelkoM01@users.noreply.github.com>
2022-08-26 20:34:44 +0300

    Add files via upload

---
+++ A1/funkcije.js
@@ -0,0 +1,18 @@
+function klik1() {
+    alert("A large solid-hoofed herbivorous ungulate mammal (Equus caballus, family Equidae, the horse family) domesticated since prehistoric times and used as a beast of burden, a draft animal, or for riding.");
+}
+function klik2() {
+    alert("Large, domesticated, cloven-hooved, herbivores. They are a prominent modern member of the subfamily Bovinae and the most widespread species of the genus Bos. Adult females are referred to as cows and adult males are referred to as bulls.");
+
+}
+function klik3() {
+    alert("The cat is a domestic species of small carnivorous mammal. It is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family.");
+}
+function klik4() {
+    alert("Chicken is the most common type of poultry in the world. Owing to the relative ease and low cost of raising chickens—in comparison to mammals such as cattle or hogs—chicken meat and chicken eggs have become prevalent in numerous cuisines.");
+
+}
+function klik5() {
+    alert("The dog or domestic dog is a domesticated descendant of the wolf. The dog is derived from an ancient, extinct wolf, and the modern wolf is the dog's nearest living relative. The dog was the first species to be domesticated, by hunter–gatherers over 15,000 years ago, before the development of agriculture.");
+
+}
